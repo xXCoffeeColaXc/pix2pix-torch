1e6eb6f1f6c90a207ed107e490a4748afb115fb2
xXCoffeeColaXc <tamsyandro@gmail.com>
2023-10-04 11:20:21 +0200

    baseline model

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
-TRAIN_DIR = "data/maps/train"
-VAL_DIR = "data/maps/val"
+TRAIN_DIR = "data/cityscapes/train"
+VAL_DIR = "data/cityscapes/val"
 LEARNING_RATE = 2e-4
 BATCH_SIZE = 16
 NUM_WORKERS = 2
@@ -14,7 +14,7 @@ L1_LAMBDA = 100
 LAMBDA_GP = 10
 NUM_EPOCHS = 200
 LOAD_MODEL = False
-SAVE_MODEL = False
+SAVE_MODEL = True
 CHECKPOINT_DISC = "disc.pth.tar"
 CHECKPOINT_GEN = "gen.pth.tar"
 
--- dataset.py
+++ dataset.py
@@ -19,8 +19,8 @@ class MapDataset(Dataset):
         img_file = self.list_files[index]
         img_path = os.path.join(self.root_dir, img_file)
         image = np.array(Image.open(img_path))
-        input_image = image[:, :600, :]
-        target_image = image[:, 600:, :]
+        target_image = image[:, :256, :]
+        input_image = image[:, 256:, :]
 
         augmentations = config.both_transform(image=input_image, image0=target_image)
         input_image = augmentations["image"]
